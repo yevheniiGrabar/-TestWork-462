2e351f6d585af8056cbc48ec3024f0ccd029b47d
r3dfox <yevheniigrabar@gmail.com>
2022-05-17 21:39:24 +0300

    minor fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
 
+    /** @var string */
     protected string $namespace = 'App\\Http\\Controllers';
 
     /**
